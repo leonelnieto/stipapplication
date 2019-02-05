08bea29e0349a423aeec3e50cfc01b380f7afc23
Leonel Nieto <43765662+leonelnieto@users.noreply.github.com>
2019-02-05 07:01:43 -0700

    Region Filters
    Added Region Filters, Need to replicate to other pages

--- js/maindb-2.0.js
+++ js/maindb-2.0.js
@@ -18,9 +18,9 @@ const formatter = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 2
   });
  //Query Dataset then build table
-function dataTableBuilder(pn_status,workshop,dom){
+function dataTableBuilder(pn_status,workshop,dom,region){
     //Build where clause by filter
-    var whereClause = whereClauseBuilder(pn_status,workshop);
+    var whereClause = whereClauseBuilder(pn_status,workshop,region);
     var query = sourceDataset+selectColumns+whereClause+tail;
     //console.log(query);
     fetch(query)
@@ -95,8 +95,8 @@ function drillPlotlyChart(pn_status,workshop,dom,groupOrder){
   });
 }
 //Drill Chart takes type parameter for table or graph
-function drillVisual(pn_status,workshop,dom,groupOrder,aggregate,type){
-    var whereClause = whereClauseBuilder(pn_status,workshop);
+function drillVisual(pn_status,workshop,dom,groupOrder,aggregate,type,region){
+    var whereClause = whereClauseBuilder(pn_status,workshop,region);
     var vizQueryAgg = "?$select="+groupOrder+",sum("+aggregate+") as aggregate";
     var vizQueryGroup = "&$group="+groupOrder;
     var vizQueryOrder = "&$order="+groupOrder;
@@ -141,28 +141,33 @@ function drillVisual(pn_status,workshop,dom,groupOrder,aggregate,type){
   });
 }
 //Helper function to build where clause
-function whereClauseBuilder(pn_status,workshop) {
+function whereClauseBuilder(pn_status,workshop,region) {
     var whereClause = "";
     if(workshop === "all"){
         workshop = "";
     } else {
-        workshop = "and workshop_cat in ("+workshop+")";
+        workshop = "and workshop_cat in ("+workshop+") ";
+    }
+    if(region === 0 || region === undefined){
+        region = "";
+    } else {
+        region = "and region_cd='"+region+"' "
     }
     switch(pn_status){
         case "unfunded":
-            whereClause = "&$where=stip_workshop='N' "+workshop;
+            whereClause = "&$where=stip_workshop='N' "+workshop+region;
         break;
         case "proposed":
-            whereClause = "&$where=stip_workshop='Y' "+workshop;
+            whereClause = "&$where=stip_workshop='Y' "+workshop+region;
         break;
         case "comapp":
-            whereClause = "&$where=comm_aprv_ind='Y'and pin_stat_nm in('STIP','Scoping','Active','Advertised','Under Construction','Substantially Compl','Physically Complete') "+workshop;
+            whereClause = "&$where=comm_aprv_ind='Y'and pin_stat_nm in('STIP','Scoping','Active','Advertised','Under Construction','Substantially Compl','Physically Complete') "+workshop+region;
         break;
         case "design":
-            whereClause = "&$where=pin_stat_nm in('STIP','Scoping','Active') "+workshop;
+            whereClause = "&$where=pin_stat_nm in('STIP','Scoping','Active') "+workshop+region;
         break;
         case "construction":
-            whereClause = "&$where=pin_stat_nm in('Advertised','Under Construction','Substantially Compl','Physically Complete','Awarded') "+workshop;
+            whereClause = "&$where=pin_stat_nm in('Advertised','Under Construction','Substantially Compl','Physically Complete','Awarded') "+workshop+region;
         break;
     }
     return whereClause;
@@ -518,3 +523,73 @@ function printSourceData(dom){
         console.log("{*_*} Bummer, could not load onepager data!!!!"+err);
     })
 }
+//Helper function to get URL Vars
+function getAllUrlParams(url) {
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    // we'll store the parameters here
+    var obj = {};
+    // if query string exists
+    if (queryString) {
+      // stuff after # is not part of query string, so get rid of it
+      queryString = queryString.split('#')[0];
+      // split our query string into its component parts
+      var arr = queryString.split('&');
+      for (var i = 0; i < arr.length; i++) {
+        // separate the keys and the values
+        var a = arr[i].split('=');
+        // set parameter name and value (use 'true' if empty)
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+        // (optional) keep case consistent
+        paramName = paramName.toLowerCase();
+        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        if (paramName.match(/\[(\d+)?\]$/)) {
+          // create key if it doesn't exist
+          var key = paramName.replace(/\[(\d+)?\]/, '');
+          if (!obj[key]) obj[key] = [];
+          // if it's an indexed array e.g. colors[2]
+          if (paramName.match(/\[\d+\]$/)) {
+            // get the index value and add the entry at the appropriate position
+            var index = /\[(\d+)\]/.exec(paramName)[1];
+            obj[key][index] = paramValue;
+          } else {
+            // otherwise add the value to the end of the array
+            obj[key].push(paramValue);
+          }
+        } else {
+          // we're dealing with a string
+          if (!obj[paramName]) {
+            // if it doesn't exist, create property
+            obj[paramName] = paramValue;
+          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+            // if property does exist and it's a string, convert it to an array
+            obj[paramName] = [obj[paramName]];
+            obj[paramName].push(paramValue);
+          } else {
+            // otherwise add the property
+            obj[paramName].push(paramValue);
+          }
+        }
+      }
+    }
+    return obj;
+}
+//Path Parser
+//This was put together in haste..... Verify and optimize
+function pathClearandReload(region){
+    var load = '';
+    //var id = '';
+    if(region === undefined){
+        load += window.location.hostname+window.location.pathname;
+       //id = 0;
+    } else {
+        load += window.location.hostname+window.location.pathname+"?region="+region;
+        //id = region;
+    }
+    window.location.href = load;
+    //var button = document.getElementById("filterregion"+id);
+    //button.classList.add("active");
+}
+  
