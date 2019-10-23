6ef5e1422cff9585004e6c93c5c8ca51600f953d
Alex Gilvarry <gilvarry@wisc.edu>
2019-10-23 07:46:15 -0600

    query
    query

--- js/maindb-2.0.js
+++ js/maindb-2.0.js
@@ -649,150 +649,158 @@ function pathClearandReload(region){
    
     window.location.href = load;
 }
-//A second version of map loader 
-function mapLoaderDynamic(dom,region,program){
+  //A second version of map loader 
+  function mapLoaderDynamic(dom,region,program){
     let centerLong = -111.693657;
     let centerLat = 39.631301;
     let zoom = 2500000;
     switch(region) {
-        case 1:
+      case 1:
         centerLong = -112.455054;
         centerLat = 41.343983;
         zoom = 700000;
         break;
-        case 2:
+      case 2:
         centerLong = -111.667910;
         centerLat = 40.680967;
         zoom = 1200000;
         break;
-        case 3:
+      case 3:
         centerLong = -111.534103;
         centerLat = 40.134867;
         zoom = 1200000;
         break;
-        case 4:
+      case 4:
         centerLong = -111.662749;
         centerLat = 38.377228;
         zoom = 1400000;
         break;
-}
-
-let mapFilter = region ? "AND REGION_CD ='"+ region +"'": "";
-
-require([
-        "esri/Map",
-        "esri/views/MapView",
-        "esri/widgets/Legend",
-        "esri/layers/FeatureLayer",
-        "esri/widgets/Expand",
-        "esri/widgets/BasemapToggle"
-    ],
-    function (Map, MapView, Legend, FeatureLayer, Expand, BasemapToggle) {
-    //symbols for year lines
-    const year2018 = {type: "simple-line", color: "#A87000", width: 4, style: "solid"};
-    const year2019 = {type: "simple-line", color: "#4ce600", width: 4, style: "solid"};
-    const year2020 = {type: "simple-line", color: "#00a9e6", width: 4, style: "solid"};
-    const year2021 = {type: "simple-line", color: "#005ce6", width: 4, style: "solid"};
-    const year2022 = {type: "simple-line", color: "#ffaa00", width: 4, style: "solid"};
-    const year2023 = {type: "simple-line", color: "#1a1a1a", width: 4, style: "solid"};
-    const year2024 = {type: "simple-line", color: "#fc30cd", width: 4, style: "solid"};
-    const year2025 = {type: "simple-line", color: "#e64c00", width: 4, style: "solid"};
-    const year2026 = {type: "simple-line", color: "#e60000", width: 4, style: "solid"};
-    const year2027 = {type: "simple-line", color: "#a87000", width: 4, style: "solid"};
-    //values for feature rendering lines
-    const STIPRender = {
-        type: "unique-value", 
-        field: "FORECAST_ST_YR",
-        uniqueValueInfos: [ // used for specifying unique values
-        {value: "2018", symbol: year2018, label: "2018" }, 
-        {value: "2019", symbol: year2019, label: "2019" },
-        {value: "2020", symbol: year2020, label: "2020" },
-        {value: "2021", symbol: year2021, label: "2021" },
-        {value: "2022", symbol: year2022, label: "2022" },
-        {value: "2023", symbol: year2023, label: "2023" },
-        {value: "2024", symbol: year2024, label: "2024" },
-        {value: "2025", symbol: year2025, label: "2025" },
-        {value: "2026", symbol: year2026, label: "2026" },
-        {value: "2027", symbol: year2027, label: "2027" }
-        ]
-    };
-    //Map Query Statements
-    const filter = ["WORKSHOP_CAT = 'Transportation Investment Funds' AND STIP_WORKSHOP_YR = '2019'", //0 TransportationInvestmentFunds - no records
-                    "WORKSHOP_CAT = 'Transportation Solutions' AND STIP_WORKSHOP_YR = '2019'",//1 TransportationSolutions
-                    "WORKSHOP_CAT = 'Contingency Fund' AND STIP_WORKSHOP_YR = '2019'", //2 ContingencyFund - no records
-                    "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'Preservation High Volume' OR WORKSHOP_CAT = 'Rehabilitation High Volume')", //3 PavementHighVolume 
-                    "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'Preservation Low Volume' OR WORKSHOP_CAT = 'Rehabilitation Low Volume')", //4 PavementLowVolume
-                    "WORKSHOP_CAT = 'Bridge Preservation' AND STIP_WORKSHOP_YR = '2019'", //5 BridgePreservation
-                    "WORKSHOP_CAT = 'Bridge Replacement and Rehabilitation' AND STIP_WORKSHOP_YR = '2019'", //6 BridgeReplacementandRehabilitation
-                    "WORKSHOP_CAT = 'HSIP - Highway Safety Improvement' AND STIP_WORKSHOP_YR = '2019'", //7 HighwaySafetyImprovement
-                    "WORKSHOP_CAT = 'SSIP - Safety Spot Improvement' AND STIP_WORKSHOP_YR = '2019'", //8 SafetySpotImprovement - I think this one is being retired
-                    "WORKSHOP_CAT = 'Barrier Treatments' AND STIP_WORKSHOP_YR = '2019'", //9 BarrierTreatments - no records
-                    "WORKSHOP_CAT = 'Small Area Lighting' AND STIP_WORKSHOP_YR = '2019'", //10 SmallAreaLighting - no records
-                    "WORKSHOP_CAT = 'Safe Routes to Schools' AND STIP_WORKSHOP_YR = '2019'", //11 SafeRoutestoSchools - no records
-                    "WORKSHOP_CAT = 'Sign Modification & Replacement' AND STIP_WORKSHOP_YR = '2019'",//12 SignModificationReplacement - no records
-                    "WORKSHOP_CAT = 'Railway-Highway Grade Crossing' AND STIP_WORKSHOP_YR = '2019'", //13 RailwayHighwayGradeCrossing
-                    "WORKSHOP_CAT = 'New Traffic Signals' AND STIP_WORKSHOP_YR = '2019'", //14 NewTrafficSignals
-                    "WORKSHOP_CAT = 'Freight' AND STIP_WORKSHOP_YR = '2019'",//15 Freight - no records
-                    "WORKSHOP_CAT = 'Off-System Bridge' AND STIP_WORKSHOP_YR = '2019'", //16 OffSystemBridge - no records
-                    "WORKSHOP_CAT = 'Non-Urban' AND STIP_WORKSHOP_YR = '2019'", //17 NonUrban - no records
-                    "WORKSHOP_CAT = 'Small Urban' AND STIP_WORKSHOP_YR = '2019'", //18 SmallUrban - no records
-                    "WORKSHOP_CAT = 'State Park Access' AND STIP_WORKSHOP_YR = '2019'", //19 StateParkAccess - no records
-                    "WORKSHOP_CAT = 'Transportation Alternatives' AND STIP_WORKSHOP_YR = '2019'", //20 TransportationAlternatives
-                    "WORKSHOP_CAT = 'Environmental Studies' AND STIP_WORKSHOP_YR = '2019'", //21 EnvironmentalStudies - no records
-                    "WORKSHOP_CAT = 'ATMS Asset Management' AND STIP_WORKSHOP_YR = '2019'", //22 ATMSAssetManagement - no records
-                    "WORKSHOP_CAT = 'Federal Lands Access Program' AND STIP_WORKSHOP_YR = '2019'", //23 FederalLandsAccessProgram - no records
-                    "WORKSHOP_CAT = 'Other' AND STIP_WORKSHOP_YR = '2019'", //24 Other - no records
-                    "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'HSIP - Highway Safety Improvement' OR WORKSHOP_CAT = 'Safe Routes to Schools' OR WORKSHOP_CAT = 'New Traffic Signals' OR WORKSHOP_CAT = 'Railway-Highway Grade Crossing')", //25 TrafficSafety - is this one redundant?
-                    "WORKSHOP_CAT = 'MPO' AND STIP_WORKSHOP_YR = '2019'", //26 LocalGovernmentMPOs no records
-                    "WORKSHOP_CAT = 'Reconstruction High Volume' AND STIP_WORKSHOP_YR = '2019'", //27 PavementLowVolume
-                    "STIP_WORKSHOP_YR = '2019'", //noquery
-                    "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'Bridge Preservation' OR WORKSHOP_CAT = 'Bridge Replacement and Rehabilitation')" //29 All Structures
-                ]  
+    }
     
-                // Expand widget for the queryFeature div
-    let html = makeQueryForm()
-    $('#map').append(html);
-        let layer = new FeatureLayer({
-        url: STIPData, // EPM STIP Service
-        renderer: STIPRender, //this gives the line styles
-        definitionExpression: filter[program]+mapFilter, //change filter to change dataset query
-        popupTemplate: {title: "{CONCEPT_DESC}", content: "{*}"} //the popup change be changed if we want
-    });
-    //initialize map
-    let map = new Map({
-            basemap: "streets-vector" 
-            
-    });
-    //create map view
-    let view = new MapView({
-            container: dom,
-            map: map,
-            center: [centerLong, centerLat], //edit to center base on lat and lon state center 39.631301,-111.693657
-            scale: zoom //larger number zooms out, smaller zooms in 
+    let mapFilter = region ? "AND REGION_CD ='"+ region +"'": "";
+
+    require([
+            "esri/Map",
+            "esri/views/MapView",
+            "esri/widgets/Legend",
+            "esri/layers/FeatureLayer",
+            "esri/widgets/Expand",
+            "esri/widgets/BasemapToggle"
+        ],
+        function (Map, MapView, Legend, FeatureLayer, Expand, BasemapToggle) {
+        //symbols for year lines
+        const year2018 = {type: "simple-line", color: "#A87000", width: 4, style: "solid"};
+        const year2019 = {type: "simple-line", color: "#4ce600", width: 4, style: "solid"};
+        const year2020 = {type: "simple-line", color: "#00a9e6", width: 4, style: "solid"};
+        const year2021 = {type: "simple-line", color: "#005ce6", width: 4, style: "solid"};
+        const year2022 = {type: "simple-line", color: "#ffaa00", width: 4, style: "solid"};
+        const year2023 = {type: "simple-line", color: "#1a1a1a", width: 4, style: "solid"};
+        const year2024 = {type: "simple-line", color: "#fc30cd", width: 4, style: "solid"};
+        const year2025 = {type: "simple-line", color: "#e64c00", width: 4, style: "solid"};
+        const year2026 = {type: "simple-line", color: "#e60000", width: 4, style: "solid"};
+        const year2027 = {type: "simple-line", color: "#a87000", width: 4, style: "solid"};
+        //values for feature rendering lines
+        const STIPRender = {
+            type: "unique-value", 
+            field: "FORECAST_ST_YR",
+            uniqueValueInfos: [ // used for specifying unique values
+            {value: "2018", symbol: year2018, label: "2018" }, 
+            {value: "2019", symbol: year2019, label: "2019" },
+            {value: "2020", symbol: year2020, label: "2020" },
+            {value: "2021", symbol: year2021, label: "2021" },
+            {value: "2022", symbol: year2022, label: "2022" },
+            {value: "2023", symbol: year2023, label: "2023" },
+            {value: "2024", symbol: year2024, label: "2024" },
+            {value: "2025", symbol: year2025, label: "2025" },
+            {value: "2026", symbol: year2026, label: "2026" },
+            {value: "2027", symbol: year2027, label: "2027" }
+            ]
+        };
+        //Map Query Statements
+        const filter = ["WORKSHOP_CAT = 'Transportation Investment Funds' AND STIP_WORKSHOP_YR = '2019'", //0 TransportationInvestmentFunds - no records
+                        "WORKSHOP_CAT = 'Transportation Solutions' AND STIP_WORKSHOP_YR = '2019'",//1 TransportationSolutions
+                        "WORKSHOP_CAT = 'Contingency Fund' AND STIP_WORKSHOP_YR = '2019'", //2 ContingencyFund - no records
+                        "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'Preservation High Volume' OR WORKSHOP_CAT = 'Rehabilitation High Volume')", //3 PavementHighVolume 
+                        "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'Preservation Low Volume' OR WORKSHOP_CAT = 'Rehabilitation Low Volume')", //4 PavementLowVolume
+                        "WORKSHOP_CAT = 'Bridge Preservation' AND STIP_WORKSHOP_YR = '2019'", //5 BridgePreservation
+                        "WORKSHOP_CAT = 'Bridge Replacement and Rehabilitation' AND STIP_WORKSHOP_YR = '2019'", //6 BridgeReplacementandRehabilitation
+                        "WORKSHOP_CAT = 'HSIP - Highway Safety Improvement' AND STIP_WORKSHOP_YR = '2019'", //7 HighwaySafetyImprovement
+                        "WORKSHOP_CAT = 'SSIP - Safety Spot Improvement' AND STIP_WORKSHOP_YR = '2019'", //8 SafetySpotImprovement - I think this one is being retired
+                        "WORKSHOP_CAT = 'Barrier Treatments' AND STIP_WORKSHOP_YR = '2019'", //9 BarrierTreatments - no records
+                        "WORKSHOP_CAT = 'Small Area Lighting' AND STIP_WORKSHOP_YR = '2019'", //10 SmallAreaLighting - no records
+                        "WORKSHOP_CAT = 'Safe Routes to Schools' AND STIP_WORKSHOP_YR = '2019'", //11 SafeRoutestoSchools - no records
+                        "WORKSHOP_CAT = 'Sign Modification & Replacement' AND STIP_WORKSHOP_YR = '2019'",//12 SignModificationReplacement - no records
+                        "WORKSHOP_CAT = 'Railway-Highway Grade Crossing' AND STIP_WORKSHOP_YR = '2019'", //13 RailwayHighwayGradeCrossing
+                        "WORKSHOP_CAT = 'New Traffic Signals' AND STIP_WORKSHOP_YR = '2019'", //14 NewTrafficSignals
+                        "WORKSHOP_CAT = 'Freight' AND STIP_WORKSHOP_YR = '2019'",//15 Freight - no records
+                        "WORKSHOP_CAT = 'Off-System Bridge' AND STIP_WORKSHOP_YR = '2019'", //16 OffSystemBridge - no records
+                        "WORKSHOP_CAT = 'Non-Urban' AND STIP_WORKSHOP_YR = '2019'", //17 NonUrban - no records
+                        "WORKSHOP_CAT = 'Small Urban' AND STIP_WORKSHOP_YR = '2019'", //18 SmallUrban - no records
+                        "WORKSHOP_CAT = 'State Park Access' AND STIP_WORKSHOP_YR = '2019'", //19 StateParkAccess - no records
+                        "WORKSHOP_CAT = 'Transportation Alternatives' AND STIP_WORKSHOP_YR = '2019'", //20 TransportationAlternatives
+                        "WORKSHOP_CAT = 'Environmental Studies' AND STIP_WORKSHOP_YR = '2019'", //21 EnvironmentalStudies - no records
+                        "WORKSHOP_CAT = 'ATMS Asset Management' AND STIP_WORKSHOP_YR = '2019'", //22 ATMSAssetManagement - no records
+                        "WORKSHOP_CAT = 'Federal Lands Access Program' AND STIP_WORKSHOP_YR = '2019'", //23 FederalLandsAccessProgram - no records
+                        "WORKSHOP_CAT = 'Other' AND STIP_WORKSHOP_YR = '2019'", //24 Other - no records
+                        "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'HSIP - Highway Safety Improvement' OR WORKSHOP_CAT = 'Safe Routes to Schools' OR WORKSHOP_CAT = 'New Traffic Signals' OR WORKSHOP_CAT = 'Railway-Highway Grade Crossing')", //25 TrafficSafety - is this one redundant?
+                        "WORKSHOP_CAT = 'MPO' AND STIP_WORKSHOP_YR = '2019'", //26 LocalGovernmentMPOs no records
+                        "WORKSHOP_CAT = 'Reconstruction High Volume' AND STIP_WORKSHOP_YR = '2019'", //27 PavementLowVolume
+                        "STIP_WORKSHOP_YR = '2019'", //noquery
+                        "STIP_WORKSHOP_YR = '2019' AND (WORKSHOP_CAT = 'Bridge Preservation' OR WORKSHOP_CAT = 'Bridge Replacement and Rehabilitation')" //29 All Structures
+                    ]  
+        
+                    // Expand widget for the queryFeature div
+    	let html = makeQueryForm()
+        $('#map').append(html);
+         let layer = new FeatureLayer({
+            url: STIPData, // EPM STIP Service
+            renderer: STIPRender, //this gives the line styles
+            definitionExpression: filter[program]+mapFilter, //change filter to change dataset query
+            popupTemplate: {title: "{CONCEPT_DESC}", content: "{*}"} //the popup change be changed if we want
         });
-    map.add(layer);	    
-    var legend = new Legend({
-        view: view,
-        layerInfos: [{
-        layer: layer,
-        title: "Legend"
-        }]
+        //initialize map
+        let map = new Map({
+                basemap: "streets-vector" 
+                
+        });
+        //create map view
+        let view = new MapView({
+                container: dom,
+                map: map,
+                center: [centerLong, centerLat], //edit to center base on lat and lon state center 39.631301,-111.693657
+                scale: zoom //larger number zooms out, smaller zooms in 
+            });
+        map.add(layer);	    
+        var legend = new Legend({
+            view: view,
+            layerInfos: [{
+            layer: layer,
+            title: "Legend"
+            }]
+        });
+        var basemapToggle = new BasemapToggle({
+            view: view,
+            nextBasemap: "satellite"
+        });
+        const filterProjects = new Expand({
+	    	expandIconClass: "esri-icon-filter",
+		    expandTooltip: "Query Projects",
+	    	expanded: false,
+	    	group: "top-left",
+	    	view: view,
+	    	content: document.getElementById("queryProjects")
+        });
+        view.ui.add(filterProjects, "top-left")
+        view.ui.add(legend, "bottom-right");
+        view.ui.add(basemapToggle, "top-right");
+
+        getFeatures(layer).then(function(data){
+            let features = data.features;
+            processFeatures(features);
+        });
+        
     });
-    var basemapToggle = new BasemapToggle({
-        view: view,
-        nextBasemap: "satellite"
-    });
-    const filterProjects = new Expand({
-        expandIconClass: "esri-icon-filter",
-        expandTooltip: "Query Projects",
-        expanded: false,
-        group: "top-left",
-        view: view,
-        content: document.getElementById("queryProjects")
-    });
-    view.ui.add(filterProjects, "top-left")
-    view.ui.add(legend, "bottom-right");
-    view.ui.add(basemapToggle, "top-right");
+  }
 
   function processFeatures(features){
       let counties = []
@@ -851,7 +859,7 @@ require([
 
   }
 
-function getFeatures(layer) {
+  function getFeatures(layer) {
     let query = layer.createQuery();
     return layer.queryFeatures(query);
 }
@@ -900,13 +908,4 @@ function getFeatures(layer) {
   </div>`
   }
 
-  for(i=0;i<document.querySelectorAll(".filterregion").length;i++){
-    document.querySelectorAll(".filterregion")[i].addEventListener("click", function(event){
-        if(event.target.attributes.region.value === "all"){
-            window.location = window.location.pathname;
-        }
-        else{
-            pathClearandReload(event.target.attributes.region.value);
-        }
-    });
-}
+  console.log(document.querySelectorAll(".regionFilter"))
