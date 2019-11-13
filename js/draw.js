c4ad5fdd0c4865729e5c189869d9ebd011093b0f
Alex Gilvarry <gilvarry@wisc.edu>
2019-11-13 16:12:29 -0700

    update charts

--- js/draw.js
+++ js/draw.js
@@ -1,8 +1,8 @@
-let region = getAllUrlParams(window.location.href)["region"];
+// let region = getAllUrlParams(window.location.href)["region"];
 
-if(region === undefined){
-    region = 0;
-}
+// if(region === undefined){
+//     region = 0;
+// }
 
 function draw(program, programIndex,region){
     dataTableBuilder('unfunded',program,'#pills-unfundedtbl',region);
@@ -16,35 +16,46 @@ function draw(program, programIndex,region){
     //assign workshop value function callback here
 }
 
-function drawProposed(agg, program){
+function drawProposed(agg){
+    let program = document.getElementById("currentProgram").getAttribute("value");
+    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
     drillVisual('proposed',program,'proposedbyYearChart','FORECAST_ST_YR',agg,'chart',region);
     drillVisual('proposed',program,'proposedbyYearTable','FORECAST_ST_YR',agg,'table',region);
     drillVisual('proposed',program,'proposedbyRegionChart','REGION_CD',agg,'chart',region);
     drillVisual('proposed',program,'proposedbyRegionTable','REGION_CD',agg,'table',region);
 }
 
-function drawComApp(agg, program) {
+function drawComApp(agg) {
+    let program = document.getElementById("currentProgram").getAttribute("value");
+    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
     drillVisual('comapp',program,'comappbyYearChart','FORECAST_ST_YR',agg,'chart',region);
     drillVisual('comapp',program,'comappbyYearTable','FORECAST_ST_YR',agg,'table',region);
     drillVisual('comapp',program,'comappbyRegionChart','REGION_CD',agg,'chart',region);
     drillVisual('comapp',program,'comappbyRegionTable','REGION_CD',agg,'table',region);
 }
 
-function drawDesign(agg, program){
+function drawDesign(agg){
+    let program = document.getElementById("currentProgram").getAttribute("value");
+    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
     drillVisual('design',program,'designbyYearChart','FORECAST_ST_YR',agg,'chart',region);
     drillVisual('design',program,'designbyYearTable','FORECAST_ST_YR',agg,'table',region);
     drillVisual('design',program,'designbyRegionChart','REGION_CD',agg,'chart',region);
     drillVisual('design',program,'designbyRegionTable','REGION_CD',agg,'table',region);
 }
 
-function drawConstruction(agg, program){
+function drawConstruction(agg){
+    let program = document.getElementById("currentProgram").getAttribute("value");
+    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
     drillVisual('construction',program,'constructionbyYearChart','FORECAST_ST_YR',agg,'chart',region);
     drillVisual('construction',program,'constructionbyYearTable','FORECAST_ST_YR',agg,'table',region);
     drillVisual('construction',program,'constructionbyRegionChart','REGION_CD',agg,'chart',region);
     drillVisual('construction',program,'constructionbyRegionTable','REGION_CD',agg,'table',region);
 }
 
-function drawUnfunded(agg, program){
+function drawUnfunded(agg){
+    let program = document.getElementById("currentProgram").getAttribute("value");
+    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
+    
     drillVisual('unfunded',program,'unfundedbyRegionChart','REGION_CD',agg,'chart',region);
     drillVisual('unfunded',program,'unfundedbyRegionTable','REGION_CD',agg,'table',region);
     drillVisual('unfunded',program,'unfunedbyYearChart','FORECAST_ST_YR',agg,'chart',region);
@@ -52,32 +63,33 @@ function drawUnfunded(agg, program){
 }
 
 for(i=0;i<document.querySelectorAll(".proposed").length;i++){
+    console.log(document.querySelectorAll(".proposed")[i])
     document.querySelectorAll(".proposed")[i].addEventListener("click", function(event){
-        
-        setTimeout(function(){drawProposed(event.target.attributes.agg.value, event.target.attributes.workshop.value)},500)
+        console.log("hi")
+        drawProposed(event.target.attributes.agg.value)
     })
 };
 
 for(i=0;i<document.querySelectorAll(".comapp").length;i++){
     document.querySelectorAll(".comapp")[i].addEventListener("click", function(event){
-        setTimeout(function(){drawComApp(event.target.attributes.agg.value, event.target.attributes.workshop.value)},500)
+        setTimeout(function(){drawComApp(event.target.attributes.agg.value)},500)
     })
 };
 
 for(i=0;i<document.querySelectorAll(".design").length;i++){
     document.querySelectorAll(".design")[i].addEventListener("click", function(event){
-        setTimeout(function(){drawDesign(event.target.attributes.agg.value, event.target.attributes.workshop.value)},500)
+        setTimeout(function(){drawDesign(event.target.attributes.agg.value)},500)
     })
 };
 
 for(i=0;i<document.querySelectorAll(".construction").length;i++){
     document.querySelectorAll(".construction")[i].addEventListener("click", function(event){
-        setTimeout(function(){drawConstruction(event.target.attributes.agg.value, event.target.attributes.workshop.value)},500)
+        setTimeout(function(){drawConstruction(event.target.attributes.agg.value)},500)
     })
 };
 
 for(i=0;i<document.querySelectorAll(".unfunded").length;i++){
     document.querySelectorAll(".unfunded")[i].addEventListener("click", function(event){
-        setTimeout(function(){drawUnfunded(event.target.attributes.agg.value, event.target.attributes.workshop.value)},500)
+        setTimeout(function(){drawUnfunded(event.target.attributes.agg.value)},500)
     })
 };
