a03018ecda5daeb21abfcff154b56ad2ed2c1edd
Alex Gilvarry <gilvarry@wisc.edu>
2019-11-14 14:17:44 -0700

    Finish drawCharts

--- js/draw.js
+++ js/draw.js
@@ -8,52 +8,6 @@ function draw(program, programIndex,region){
     mapLoaderDynamic(parseInt(region),programIndex);
 };
 
-function drawProposed(agg){
-    let program = document.getElementById("currentProgram").getAttribute("value");
-    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
-    drillVisual('proposed',program,'proposedbyYearChart','FORECAST_ST_YR',agg,'chart',region);
-    drillVisual('proposed',program,'proposedbyYearTable','FORECAST_ST_YR',agg,'table',region);
-    drillVisual('proposed',program,'proposedbyRegionChart','REGION_CD',agg,'chart',region);
-    drillVisual('proposed',program,'proposedbyRegionTable','REGION_CD',agg,'table',region);
-};
-
-function drawComApp(agg) {
-    let program = document.getElementById("currentProgram").getAttribute("value");
-    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
-    drillVisual('comapp',program,'comappbyYearChart','FORECAST_ST_YR',agg,'chart',region);
-    drillVisual('comapp',program,'comappbyYearTable','FORECAST_ST_YR',agg,'table',region);
-    drillVisual('comapp',program,'comappbyRegionChart','REGION_CD',agg,'chart',region);
-    drillVisual('comapp',program,'comappbyRegionTable','REGION_CD',agg,'table',region);
-};
-
-function drawDesign(agg){
-    let program = document.getElementById("currentProgram").getAttribute("value");
-    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
-    drillVisual('design',program,'designbyYearChart','FORECAST_ST_YR',agg,'chart',region);
-    drillVisual('design',program,'designbyYearTable','FORECAST_ST_YR',agg,'table',region);
-    drillVisual('design',program,'designbyRegionChart','REGION_CD',agg,'chart',region);
-    drillVisual('design',program,'designbyRegionTable','REGION_CD',agg,'table',region);
-};
-
-function drawConstruction(agg){
-    let program = document.getElementById("currentProgram").getAttribute("value");
-    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
-    drillVisual('construction',program,'constructionbyYearChart','FORECAST_ST_YR',agg,'chart',region);
-    drillVisual('construction',program,'constructionbyYearTable','FORECAST_ST_YR',agg,'table',region);
-    drillVisual('construction',program,'constructionbyRegionChart','REGION_CD',agg,'chart',region);
-    drillVisual('construction',program,'constructionbyRegionTable','REGION_CD',agg,'table',region);
-};
-
-function drawUnfunded(agg){
-    let program = document.getElementById("currentProgram").getAttribute("value");
-    let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
-
-    drillVisual('unfunded',program,'unfundedbyRegionChart','REGION_CD',agg,'chart',region);
-    drillVisual('unfunded',program,'unfundedbyRegionTable','REGION_CD',agg,'table',region);
-    drillVisual('unfunded',program,'unfunedbyYearChart','FORECAST_ST_YR',agg,'chart',region);
-    drillVisual('unfunded',program,'unfunedbyYearTable','FORECAST_ST_YR',agg,'table',region);
-};
-
 function drawCharts(agg,projPhase){
     let program = document.getElementById("currentProgram").getAttribute("value");
     let region = parseInt(document.getElementById("currentRegion").getAttribute("value"));
@@ -96,14 +50,13 @@ for(i=0;i<document.querySelectorAll(".unfunded").length;i++){
 
 for(i=0;i<document.querySelectorAll(".proposed").length;i++){
     document.querySelectorAll(".proposed")[i].addEventListener("click", function(event){
-        ddrawCharts(event.target.attributes.agg.value, "proposed");
+        drawCharts(event.target.attributes.agg.value, "proposed");
         addSelectedFinance(event,".proposed");
     })
 };
 
 for(i=0;i<document.querySelectorAll(".comapp").length;i++){
-    document.querySelectorAll(".comapp")[i].addEventListener("click", function(event){
-        
+    document.querySelectorAll(".comapp")[i].addEventListener("click", function(event){        
         drawCharts(event.target.attributes.agg.value, "comapp");
         addSelectedFinance(event,".comapp");
     })
